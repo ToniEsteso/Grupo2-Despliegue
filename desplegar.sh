151ae973657204c4691b1344a30616e8f3523afa
JOSE MIGUEL ZOMEÑO ESCOBAR <20499963v@AD.FPMISLATA.COM>
2020-02-11 09:28:57 +0100

    hechos scripts despliegue

--- desplegar.sh
+++
@@ -1,7 +0,0 @@
-parametro=$1
-rm -rf /opt/veganfood
-mkdir /opt/veganfood
-git clone https://github.com/ToniEsteso/Veganfood.git /opt/veganfood
-cd ./veganfood
-mkdir ./html ./html/frontend ./html/backend ./mysql
-sh desplegarTodo.sh parametro
---
+++ desplegarVeganfood.sh
@@ -0,0 +1,5 @@
+parametro=$1
+git clone https://github.com/ToniEsteso/Veganfood.git 
+cd ./veganfood
+mkdir ./html ./html/frontend ./html/backend ./mysql
+sh desplegarTodo.sh parametro
