2911265c640a13903022e826272c293ef25d1b00
Carlos <chipi3011@gmail.com>
2020-02-13 07:42:33 +0000

    cambiados nombres de los contenedores

--- desplegarAdmin.sh
+++ desplegarAdmin.sh
@@ -14,7 +14,7 @@ chmod -R 777 ./html/frontend/
 npm install --prefix ./html/frontend/admin/
 npm run build --prefix ./html/frontend/admin/
 if [ $parametro = "preproduccion" ] || [ $parametro = "Preproduccion" ] || [ $parametro = "PREPRODUCCION" ]; then
-sudo docker container run -d --name veganfood_frontend -v /opt/veganfood/html/frontend:/usr/local/apache2/htdocs -p 10330:80 httpd:2.4.41
+sudo docker container run -d --name veganfood_admin -v /opt/veganfood/html/frontend/admin:/usr/local/apache2/htdocs -p 10330:80 httpd:2.4.41
 elif [ $parametro = "produccion" ] || [ $parametro = "produccion" ] || [ $parametro = "PRODUCCION" ]; then
-    docker container run -d --name veganfood_frontend -v /opt/veganfood/html/frontend:/usr/local/apache2/htdocs --expose 80 -e VIRTUAL_HOST=www.admin.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.admin.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
+    docker container run -d --name veganfood_admin -v /opt/veganfood/html/frontend/admin:/usr/local/apache2/htdocs --expose 80 -e VIRTUAL_HOST=www.admin.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.admin.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
 fi
