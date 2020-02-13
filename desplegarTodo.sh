c6ea0850152e90f25ebcc0a1c9ba268279079ff0
ToniEsteso <50195355+ToniEsteso@users.noreply.github.com>
2020-02-13 09:20:00 +0100

    fix(#4): admin quitado del script

--- desplegarTodo.sh
+++ desplegarTodo.sh
@@ -4,7 +4,6 @@ sh desplegarServidor.sh $parametro
 echo "<---------------Desplegado backend--------------->"
 sh desplegarMysql.sh
 echo "<---------------Desplegada BD--------------->"
-sh desplegarAdmin.sh $parametro
-echo "<---------------Desplegado Admin--------------->"
+
 sh desplegarCliente.sh $parametro
 echo "<---------------Desplegado frontend--------------->"
