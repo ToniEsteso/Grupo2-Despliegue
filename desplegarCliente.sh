187c142ffccc2f6c46d7ee49463979b6d4764174
ToniEsteso <50195355+ToniEsteso@users.noreply.github.com>
2020-02-13 09:26:02 +0100

    fix(#5): actualizados los permisos

--- desplegarCliente.sh
+++ desplegarCliente.sh
@@ -1,27 +1,27 @@
 #!usr/env/bin bash
 parametro=$1
-docker container stop veganfood_frontend
-docker container rm veganfood_frontend
-rm -rf ./html/frontend/
-mkdir -p ./html/frontend/Grupo2-Cliente
+sudo docker container stop veganfood_frontend
+sudo docker container rm veganfood_frontend
+sudo rm -rf ./html/frontend/
+sudo mkdir -p ./html/frontend/Grupo2-Cliente
 if [ $parametro = "preproduccion" ] || [ $parametro = "Preproduccion" ] || [ $parametro = "PREPRODUCCION" ]; then
-    git clone -b develop https://github.com/ToniEsteso/Grupo2-Cliente.git ./html/frontend/Grupo2-Cliente
+    sudo git clone -b develop https://github.com/ToniEsteso/Grupo2-Cliente.git ./html/frontend/Grupo2-Cliente
 elif [ $parametro = "produccion" ] || [ $parametro = "produccion" ] || [ $parametro = "PRODUCCION" ]; then
-    git clone https://github.com/ToniEsteso/Grupo2-Cliente.git ./html/frontend/Grupo2-Cliente
+    sudo git clone https://github.com/ToniEsteso/Grupo2-Cliente.git ./html/frontend/Grupo2-Cliente
 fi
 echo "<---------------Frontend clonado--------------->"
-chmod -R 777 ./html/frontend/
-rm -rf ./html/frontend/Grupo2-Cliente/node_modules
-npm install --prefix ./html/frontend/Grupo2-Cliente/
-chmod -R 777 ./html/frontend/
-npm run build --prefix ./html/frontend/Grupo2-Cliente/
-mkdir ./html/frontend/web
-cp -R ./html/frontend/Grupo2-Cliente/dist/* ./html/frontend/web
-cp ./html/frontend/Grupo2-Cliente/dist/.* ./html/frontend/web
-rm -rf ./html/frontend/Grupo2-Cliente
+sudo chmod -R 777 ./html/frontend/
+sudo rm -rf ./html/frontend/Grupo2-Cliente/node_modules
+sudo npm install --prefix ./html/frontend/Grupo2-Cliente/
+sudo chmod -R 777 ./html/frontend/
+sudo npm run build --prefix ./html/frontend/Grupo2-Cliente/
+sudo mkdir ./html/frontend/web
+sudo cp -R ./html/frontend/Grupo2-Cliente/dist/* ./html/frontend/web
+sudo cp ./html/frontend/Grupo2-Cliente/dist/.* ./html/frontend/web
+sudo rm -rf ./html/frontend/Grupo2-Cliente
 if [ $parametro = "preproduccion" ] || [ $parametro = "Preproduccion" ] || [ $parametro = "PREPRODUCCION" ]; then
-    sudo docker container run -d --name veganfood_frontend -v /opt/veganfood/html/frontend/web:/usr/local/apache2/htdocs -p 10320:80 httpd:2.4.41
+    sudo sudo docker container run -d --name veganfood_frontend -v /opt/veganfood/html/frontend/web:/usr/local/apache2/htdocs -p 10320:80 httpd:2.4.41
 elif [ $parametro = "produccion" ] || [ $parametro = "produccion" ] || [ $parametro = "PRODUCCION" ]; then
-    sudo docker container run -d --name veganfood_frontend -v /opt/veganfood/html/frontend/web:/usr/local/apache2/htdocs --expose 80 -e VIRTUAL_HOST=www.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
+    sudo sudo docker container run -d --name veganfood_frontend -v /opt/veganfood/html/frontend/web:/usr/local/apache2/htdocs --expose 80 -e VIRTUAL_HOST=www.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
 fi
-docker exec -it veganfood_frontend sed -i "s/AllowOverride None/AllowOverride All/g" conf/httpd.conf
+sudo docker exec -it veganfood_frontend sed -i "s/AllowOverride None/AllowOverride All/g" conf/httpd.conf
